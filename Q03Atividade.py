50771958dc67ba6e1bb4e665b9051c659ab77ffc
vmachado <silvamatheus903@gmail.com>
2023-11-30 00:23:31 -0300

    A

--- Q03Atividade.py
+++ Q03Atividade.py
@@ -1,59 +1,34 @@
+# Dicionário para armazenar endereços IP e listas de tamanhos de objeto
+dicionario = {}
 lista = []
 
+# Abrir o arquivo
 with open('apache.logs.txt', 'r') as arquivo:
+    # Iterar sobre cada linha do arquivo
     for linha in arquivo:
+        # Remover espaços em branco no início e no final da linha
         linha = linha.strip()
+
+        # Dividir a linha em partes usando espaços como delimitadores
         partes = linha.split()
 
+        # Obter o endereço IP 
         contagem_ip = partes[0]
-        if contagem_ip not in lista:
-            lista.append(contagem_ip)
-        print (partes)
 
+        # Obter o tamanho do objeto  índice 9)
+        tamanho_objeto = partes[9]
 
+        # Adicionar o tamanho do objeto à lista associada ao endereço IP no dicionário
+        if contagem_ip not in dicionario:
+            dicionario[contagem_ip] = [tamanho_objeto]
+        else:
+            dicionario[contagem_ip].append(tamanho_objeto)
 
-# Criar um dicionário onde as chaves são os endereços IP únicos
-dicionario = {ip: None for ip in lista}
+# Imprime as primeiras 5 entradas do dicionário
+for chave, valores in list(dicionario.items())[:5]:
+    print 
+    print(f'Endereço IP: {chave}, Tamanhos do objeto: {valores}')
 
-# Imprimir o dicionário final
-
-
-       
-       
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endereco_ip = partes[0]
-#identd = partes[1]
-#user = partes[2]
-#data_e_hora = partes[3] + '  ' + partes[4] 
-#http = partes[5] + '  ' + partes[6] + '  ' + partes[7]
-#codigo_status_http = partes[8]
-#tamanho_do_objeto = partes[9]
-        
-   
-    
+# Imprime o número de endereços IP encontrados
+numero_enderecos_ip = len(dicionario)
+print(f'Número de endereços IP únicos: {numero_enderecos_ip}')
