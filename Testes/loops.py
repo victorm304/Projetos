ecc2ba5ffc677e329575978c8b6e192ac0cfc537
Victor Machado <silvamatheus903@gmail.com>
2023-12-06 22:45:15 -0300

    .

--- Testes/loops.py
+++ Testes/loops.py
@@ -1,2 +1,12 @@
-num = 
-        
+nome = None
+
+while True:
+    print('Digite seu nome, ou x para parar')
+    nome = input()
+    if nome == 'x' or nome == 'X':
+        print('Encerrado')
+        break
+
+    print(f'Bem vindo, {nome}')
+
+
