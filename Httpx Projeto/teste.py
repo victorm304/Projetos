f51e8b801b9f92f66d92e3162c0f2af7d56396ca
Victor Machado <silvamatheus903@gmail.com>
2023-12-18 23:55:55 -0300

    .

--- Httpx Projeto/teste.py
+++ Httpx Projeto/teste.py
@@ -1,5 +1,13 @@
 import httpx
+import json
 
-r = httpx.post('https://httpbin.org/post', data={'key': 'value'})
+city_name = 'Rio de janeiro'
+country_code = 'br'
+lang = 'pt_br'
+api_key = '032490db50ef4537bce870ec46b033ad'
 
-print(r.url)
+
+URL = httpx.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&lang={lang}&appid={api_key}')
+result = URL.json()
+print(result['weather'])
+print(result['dt'])
