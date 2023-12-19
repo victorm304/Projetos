c4ab09afa66c7a2e072614d72388b44f97028dcb
Victor Machado <silvamatheus903@gmail.com>
2023-12-19 20:59:32 -0300

    .

--- Httpx Projeto/teste.py
+++ Httpx Projeto/teste.py
@@ -1,13 +1,15 @@
 import httpx
 import json
 
+def converter_para_celsius(kelvin):
+    celsius = kelvin - 273.15
+    return celsius
+
 city_name = 'Rio de janeiro'
 country_code = 'br'
 lang = 'pt_br'
 api_key = '032490db50ef4537bce870ec46b033ad'
 
-
 URL = httpx.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&lang={lang}&appid={api_key}')
 result = URL.json()
-print(result['weather'])
-print(result['dt'])
+print(URL)
